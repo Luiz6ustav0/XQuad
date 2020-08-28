06bdb2ba0c706687efc9b8103458fc36fed242be
Luiz Almeida <luizorst@hotmail.com>
2020-08-28 01:31:50 -0300

    Add new mouse event test

--- OpenCVplaylist/mouse_events.py
+++ OpenCVplaylist/mouse_events.py
@@ -9,12 +9,22 @@ def click_event(event, x, y, flags, param):
         cv2.putText(img, str((x, y)), (x, y), font, .5, (255, 255, 0), 2)
         cv2.imshow('image', img)
 
+def click_event_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 3, (255, 0, 255), -1)
+        points.append((x, y))
+        if len(points) >= 2:
+            cv2.line(img, points[-1], points[-2], (255, 0, 0), 5)
+        cv2.imshow('image', img)
 
-# img = np.zeros((720, 1280), np.uint8)
-img = cv2.imread('jp.png')
+
+img = np.zeros((720, 1280), np.uint8)
+# img = cv2.imread('jp.png')
 cv2.imshow('image', img)
 
-cv2.setMouseCallback('image', click_event)
+points = []
+
+cv2.setMouseCallback('image', click_event_circle)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows
