ab10e1eef4d80d27cc1b5f00cd3936da78c4d48f
Luiz Almeida <luizorst@hotmail.com>
2020-08-29 01:24:57 -0300

    _

--- OpenCVplaylist/bitwise_operations.py
+++ OpenCVplaylist/bitwise_operations.py
@@ -7,9 +7,12 @@ img1 = cv2.rectangle(img1, (200,0), (300,100), (255, 255, 255), -1)
 img2 = np.zeros((250, 500, 3), np.uint8)
 img2 = cv2.rectangle(img2, (0,500), (250,0), (255, 255, 255), -1)
 
-bitAnd = cv2.bitwise_and(img2, img1)
+# bitAnd = cv2.bitwise_and(img2, img1)
+# bitOr = cv2.bitwise_or(img2, img1)
+# bitXOr = cv2.bitwise_xor(img2, img1)
+bitNot = cv2.bitwise_not(img2)
 
-cv2.imshow('bitAnd', bitAnd)
+cv2.imshow('bitwise operations', bitNot)
 cv2.imshow('img', img1)
 
 cv2.waitKey()
